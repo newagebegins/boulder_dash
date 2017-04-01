7e840866ccd795e01ac4136d0967cd5d1a3241fe
newagebegins <newagebegins@gmail.com>
2017-04-01 13:47:10 +1000

    draw tiles

--- main.c
+++ main.c
@@ -5,9 +5,64 @@
 #include <assert.h>
 #include <stdint.h>
 
+#define ARRAY_LENGTH(array) (sizeof(array)/sizeof(*array))
 #define PI 3.14159265358979323846f
 
-#define ARRAY_LENGTH(array) (sizeof(array)/sizeof(*array))
+#define TILE_WIDTH 16
+#define TILE_HEIGHT TILE_WIDTH
+#define BACKBUFFER_WIDTH 256
+#define BACKBUFFER_HEIGHT 192
+#define BACKBUFFER_PIXEL_COUNT BACKBUFFER_WIDTH*BACKBUFFER_HEIGHT
+#define SPRITE_ATLAS_WIDTH 256
+#define MAP_HEIGHT 2
+#define MAP_WIDTH 3
+
+typedef enum {
+  Tile_earth,
+  Tile_Gem,
+  Tile_wall,
+  Tile_brick,
+  Tile_count,
+} Tile;
+
+uint32_t *backbuffer;
+uint32_t *spriteAtlas;
+int cameraX;
+int cameraY;
+
+void drawSprite(int bbX, int bbY, int atlX, int atlY) {
+  for (int y = 0; y < TILE_HEIGHT; ++y) {
+    for (int x = 0; x < TILE_WIDTH; ++x) {
+      int srcX = atlX + x;
+      int srcY = atlY + y;
+      int dstX = bbX + x;
+      int dstY = bbY + y;
+      backbuffer[dstY*BACKBUFFER_WIDTH + dstX] = spriteAtlas[srcY*SPRITE_ATLAS_WIDTH + srcX];
+    }
+  }
+}
+
+void drawTile(Tile tile, int col, int row) {
+  int atlX = 0;
+  int atlY = 0;
+
+  switch (tile) {
+    case Tile_earth:
+      atlX = 16;
+      atlY = 32;
+      break;
+
+    case Tile_brick:
+      atlX = 16;
+      atlY = 128;
+      break;
+  }
+
+  int x = col * TILE_WIDTH - cameraX;
+  int y = row * TILE_HEIGHT - cameraY;
+
+  drawSprite(x, y, atlX, atlY);
+}
 
 LRESULT CALLBACK wndProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam) {
   switch (msg) {
@@ -20,18 +75,29 @@ LRESULT CALLBACK wndProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam) {
   return 0;
 }
 
-int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdShow) {
-  int backBufferWidth = 256;
-  int backBufferHeight = 192;
-  int *backBuffer = malloc(backBufferWidth * backBufferHeight * sizeof(*backBuffer));
+char *level1 =
+  "#################\n"
+  "#...... ..$.o ..#\n"
+  "#.oRo...... ....#\n"
+  "#.......... .. .#\n"
+  "#o.  ...........#\n"
+  "#o.oo...........#\n"
+  "#...o..o........#\n"
+  "#===============#\n"
+  "#. ...o..$. ..o.#\n"
+  "#..$.....o..... #\n"
+  "#################";
 
-  BITMAPINFO backBufferBitmapInfo = {0};
-  backBufferBitmapInfo.bmiHeader.biSize = sizeof(backBufferBitmapInfo.bmiHeader);
-  backBufferBitmapInfo.bmiHeader.biWidth = backBufferWidth;
-  backBufferBitmapInfo.bmiHeader.biHeight = backBufferHeight;
-  backBufferBitmapInfo.bmiHeader.biPlanes = 1;
-  backBufferBitmapInfo.bmiHeader.biBitCount = 32;
-  backBufferBitmapInfo.bmiHeader.biCompression = BI_RGB;
+int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdShow) {
+  backbuffer = malloc(BACKBUFFER_WIDTH * BACKBUFFER_HEIGHT * sizeof(*backbuffer));
+
+  BITMAPINFO backbufferBmpInf = {0};
+  backbufferBmpInf.bmiHeader.biSize = sizeof(backbufferBmpInf.bmiHeader);
+  backbufferBmpInf.bmiHeader.biWidth = BACKBUFFER_WIDTH;
+  backbufferBmpInf.bmiHeader.biHeight = -BACKBUFFER_HEIGHT;
+  backbufferBmpInf.bmiHeader.biPlanes = 1;
+  backbufferBmpInf.bmiHeader.biBitCount = 32;
+  backbufferBmpInf.bmiHeader.biCompression = BI_RGB;
 
   WNDCLASS wndClass = {0};
   wndClass.style = CS_HREDRAW | CS_VREDRAW;
@@ -41,8 +107,8 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
   wndClass.lpszClassName = "Boulder Dash";
   RegisterClass(&wndClass);
 
-  int windowWidth = backBufferWidth*3;
-  int windowHeight = backBufferHeight*3;
+  int windowWidth = BACKBUFFER_WIDTH*3;
+  int windowHeight = BACKBUFFER_HEIGHT*3;
 
   RECT crect = {0};
   crect.right = windowWidth;
@@ -85,14 +151,25 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
   int imageHeight = *(int*)(fileContents + 22);
   uint8_t *pixels = fileContents + pixelsOffset;
 
-  int *texture = malloc(imageWidth * imageHeight * sizeof(*texture));
-  for (int i = 0, j = 0; i < imageWidth*imageHeight*3; i += 3, ++j) {
-    int red = pixels[i] << 0;
-    int green = pixels[i+1] << 8;
-    int blue = pixels[i+2] << 16;
-    int color = red | green | blue;
-    texture[j] = color;
+  spriteAtlas = malloc(imageWidth * imageHeight * sizeof(*spriteAtlas));
+
+  for (int dstRow = 0; dstRow < imageHeight; ++dstRow) {
+    int srcRow = imageHeight - dstRow - 1;
+    uint32_t *dst = spriteAtlas + dstRow*imageWidth;
+    uint8_t *src = pixels + srcRow*imageWidth*3;
+    for (int i = 0, j = 0; i < imageWidth*3; i += 3, ++j) {
+      int red   = *(src + i + 0) << 0;
+      int green = *(src + i + 1) << 8;
+      int blue  = *(src + i + 2) << 16;
+      int color = red | green | blue;
+      *(dst + j) = color;
+    }
   }
+
+  Tile map[MAP_HEIGHT][MAP_WIDTH] = {
+    {Tile_earth, Tile_earth, Tile_earth},
+    {Tile_earth, Tile_brick, Tile_earth},
+  };
 
   while (running) {
     prefcPrev = perfc;
@@ -126,18 +203,18 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
       }
     }
 
-    for (int i = 0; i < backBufferWidth * backBufferHeight; ++i) {
-      backBuffer[i] = 0xFF00FF00;
+    for (int i = 0; i < BACKBUFFER_PIXEL_COUNT; ++i) {
+      backbuffer[i] = 0xFF00FF00;
     }
 
-    for (int srcY = 0, destY = 0; srcY < imageHeight; ++srcY, ++destY) {
-      for (int srcX = 0, destX = 0; srcX < imageWidth; ++srcX, ++destX) {
-        backBuffer[destY*backBufferWidth + destX] = texture[srcY*imageWidth + srcX];
+    for (int row = 0; row < MAP_HEIGHT; ++row) {
+      for (int col = 0; col < MAP_WIDTH; ++col) {
+        drawTile(map[row][col], col, row);
       }
     }
 
     StretchDIBits(deviceContext, 0, 0, windowWidth, windowHeight,
-                  0, 0, backBufferWidth, backBufferHeight, backBuffer,
-                  &backBufferBitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+                  0, 0, BACKBUFFER_WIDTH, BACKBUFFER_HEIGHT, backbuffer,
+                  &backbufferBmpInf, DIB_RGB_COLORS, SRCCOPY);
   }
 }
