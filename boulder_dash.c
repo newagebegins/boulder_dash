4709465c3aada6feea2134a89123dd3dcbe9035f
newagebegins <newagebegins@gmail.com>
2017-05-01 21:59:00 +1000

    wip

--- boulder_dash.c
+++ boulder_dash.c
@@ -1,9 +1,67 @@
 #include <windows.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include "bitmaps.h"
+#include "caves.h"
+
+#define TILE_SIZE (2*SPRITE_SIZE)
+#define BORDER_SIZE TILE_SIZE
+
+// Viewport is the whole screen area except the border.
+#define VIEWPORT_WIDTH 256
+#define VIEWPORT_HEIGHT 192
+
+#define BACKBUFFER_WIDTH (VIEWPORT_WIDTH + BORDER_SIZE*2)
+#define BACKBUFFER_HEIGHT (VIEWPORT_HEIGHT + BORDER_SIZE*2)
+
+#define WINDOW_SCALE 3
+#define WINDOW_WIDTH (BACKBUFFER_WIDTH * WINDOW_SCALE)
+#define WINDOW_HEIGHT (BACKBUFFER_HEIGHT * WINDOW_SCALE)
+
+LRESULT CALLBACK wndProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+  switch (msg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+    default:
+      return DefWindowProc(wnd, msg, wparam, lparam);
+  }
+  return 0;
+}
 
 int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdShow) {
-  UNREFERENCED_PARAMETER(inst);
   UNREFERENCED_PARAMETER(prevInst);
   UNREFERENCED_PARAMETER(cmdLine);
-  UNREFERENCED_PARAMETER(cmdShow);
+
+  BITMAPINFO backbufferBmpInf = {0};
+  backbufferBmpInf.bmiHeader.biSize = sizeof(backbufferBmpInf.bmiHeader);
+  backbufferBmpInf.bmiHeader.biWidth = BACKBUFFER_WIDTH;
+  backbufferBmpInf.bmiHeader.biHeight = -BACKBUFFER_HEIGHT;
+  backbufferBmpInf.bmiHeader.biPlanes = 1;
+  backbufferBmpInf.bmiHeader.biBitCount = 32;
+  backbufferBmpInf.bmiHeader.biCompression = BI_RGB;
+
+  WNDCLASS wndClass = {0};
+  wndClass.style = CS_HREDRAW | CS_VREDRAW;
+  wndClass.lpfnWndProc = wndProc;
+  wndClass.hInstance = inst;
+  wndClass.hCursor = LoadCursor(0, IDC_ARROW);
+  wndClass.lpszClassName = "Boulder Dash";
+  RegisterClass(&wndClass);
+
+  RECT crect = {0};
+  crect.right = WINDOW_WIDTH;
+  crect.bottom = WINDOW_HEIGHT;
+
+  DWORD wndStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
+  AdjustWindowRect(&crect, wndStyle, 0);
+
+  HWND wnd = CreateWindowEx(0, wndClass.lpszClassName, "Boulder Dash", wndStyle, 300, 100,
+                            crect.right - crect.left, crect.bottom - crect.top,
+                            0, 0, inst, 0);
+  ShowWindow(wnd, cmdShow);
+  UpdateWindow(wnd);
+
   return 0;
 }
