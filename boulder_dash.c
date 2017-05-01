a4e9ffddc5cbac57c9cb142b9c68bb89fd0c18bd
newagebegins <newagebegins@gmail.com>
2017-05-01 21:34:33 +1000

    embed bitmaps

---
+++ boulder_dash.c
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdShow) {
+  UNREFERENCED_PARAMETER(inst);
+  UNREFERENCED_PARAMETER(prevInst);
+  UNREFERENCED_PARAMETER(cmdLine);
+  UNREFERENCED_PARAMETER(cmdShow);
+  return 0;
+}
